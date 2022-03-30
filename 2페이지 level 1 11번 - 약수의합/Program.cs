89090ae24b456ab5eab9c4740a23b04b632dfdfd
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:22:18 +0900

    약수의합

---
+++ 2페이지 level 1 11번 - 약수의합/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_11번___약수의합
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int answer = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                if(n % i == 0)
+                {
+                    answer += i;
+                }
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
