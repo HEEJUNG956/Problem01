34fe59381b340e0128a18234eedec725cdc8e76e
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 15:25:56 +0900

    정수내림차순으로배치하기

---
+++ 2페이지 level 1 7번 - 정수내림차순으로배치하기/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_7번___정수내림차순으로배치하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long answer = 0;
+            char[] a = n.ToString().ToCharArray();
+            Array.Sort(a);
+            Array.Reverse(a);
+            answer = Convert.ToInt32(new String(a));
+            Console.WriteLine(answer);
+        }
+    }
+}
