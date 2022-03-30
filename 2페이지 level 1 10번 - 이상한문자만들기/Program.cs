7252f9b8b052d86cc01e2c9f1357fc4878dd9d23
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:16:21 +0900

    이상한문자만들기

---
+++ 2페이지 level 1 10번 - 이상한문자만들기/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_10번___이상한문자만들기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int num = 0;
+            string answer = "";
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                answer += num % 2 == 0 ? s[i].ToString().ToUpper() : s[i].ToString().ToLower();
+                num = s[i] == ' ' ? 0 : num + 1;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
