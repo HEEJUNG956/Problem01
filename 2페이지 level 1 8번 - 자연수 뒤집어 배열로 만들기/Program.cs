1720468264b4be0e24ca1a036c91af97551d608d
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 15:34:36 +0900

    입력된수뒤집어 배열로만들

---
+++ 2페이지 level 1 8번 - 자연수 뒤집어 배열로 만들기/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_8번___자연수_뒤집어_배열로_만들기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            string temp = new string(n.ToString().ToCharArray().Reverse().ToArray());
+            int[] answer = new int[temp.Length];
+
+            for(int i = 0; i < temp.Length; i++)
+            {
+                answer[i] = int.Parse(temp[i].ToString());
+                Console.Write(answer[i] + " ");
+            }
+        }
+    }
+}
