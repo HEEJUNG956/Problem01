426d633387a6cddba6493cc5f36a6590997be258
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 16:02:59 +0900

    자릿수더하기

---
+++ 2페이지 level 1 9번 - 자릿수더하기/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_9번___자릿수더하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int answer = 0;
+
+            while(n > 0)
+            {
+                answer += n % 10;
+                n = n / 10;
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
