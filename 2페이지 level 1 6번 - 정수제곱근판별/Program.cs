3f8c6a306506e6717fe08581454db18cda5c68c2
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-03-30 15:09:14 +0900

    정수제곱근판별

---
+++ 2페이지 level 1 6번 - 정수제곱근판별/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_6번___정수제곱근판별
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long answer = 0;
+
+            var a = Math.Sqrt(n);
+
+            if(a % 1.0 != 0)
+            {
+                Console.WriteLine(-1);
+            }
+            else
+            {
+                answer = (long)a;
+                answer += 1;
+                answer *= answer;
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
